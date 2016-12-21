2db15a811082a1bcc186a2a2314e211fe7002045
Marcos Castro <mcastrosouza@live.com>
2016-12-21 07:39:33 -0300

    adicionado hash as senhas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,3 +1,4 @@
+import bcrypt
 from app import app
 from app.models.tables import User
 from bottle import template, static_file, request, redirect
@@ -28,29 +29,43 @@ def cadastro():
 	return template('cadastro', existe_username=False)
 
 @app.route('/cadastro', method='POST')
-def acao_cadastro(db):
+def acao_cadastro(db, session):
 	username = request.forms.get('username')
-	password = request.forms.get('password')
 	try:
 		db.query(User).filter(User.username == username).one()
 		existe_username = True
 	except:
 		existe_username = False
 	if not existe_username:
-		new_user = User(username, password)
+		password = request.forms.get('password')
+		password_bytes = str.encode(password)
+		salt_bytes = bcrypt.gensalt()
+		salt = salt_bytes.decode()
+		hashed_bytes = bcrypt.hashpw(password_bytes, salt_bytes)
+		hashed = hashed_bytes.decode()
+		new_user = User(username, hashed, salt)
 		db.add(new_user)
+		session['name'] = username
 		return redirect('/usuarios')
 	return template('cadastro', existe_username=True)
 
 @app.route('/', method='POST')
 def acao_login(db, session):
 	username = request.forms.get('username')
-	password = request.forms.get('password')
-	result = db.query(User).filter((User.username == username) \
-			& (User.password == password)).all()
-	if result:
-		session['name'] = username
-		return redirect('/usuarios')
+	try:
+		user = db.query(User).filter(User.username == username).one()
+		existe_username = True
+	except:
+		existe_username = False
+	if existe_username:
+		password = request.forms.get('password')
+		password_bytes = str.encode(password)
+		salt_bytes = str.encode(user.salt)
+		hashed_bytes = bcrypt.hashpw(password_bytes, salt_bytes)
+		hashed = hashed_bytes.decode()
+		if user.hashed == hashed:
+			session['name'] = username
+			return redirect('/usuarios')
 	return template('login', sucesso=False)
 
 @app.route('/usuarios')
--- app/models/tables.py
+++ app/models/tables.py
@@ -4,12 +4,14 @@ from app import Base
 class User(Base):
 	__tablename__ = 'users'
 	id = Column(Integer, primary_key=True)
-	username = Column(String(30), unique=True, nullable=False)
-	password = Column(String(30), nullable=False)
+	username = Column(String(50), unique=True, nullable=False)
+	hashed = Column(String(50), nullable=False)
+	salt = Column(String(50), nullable=False)
 
-	def __init__(self, username, password):
+	def __init__(self, username, hashed, salt):
 		self.username = username
-		self.password = password
+		self.hashed = hashed
+		self.salt = salt
 
 	def __repr__(self):
 		return '<User: %s>' % self.username
